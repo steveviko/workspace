b879e9ce2118e7eafdd4a98ab7b246a4bd38f878
steveviko <stevenviko@gmail.com>
2021-06-21 22:14:32 +0000

    Refactor

--- src/App.js
+++ src/App.js
@@ -99,28 +99,31 @@ const movies = {
 
 class App extends Component {
   render() {
+    /*Return JSX*/
     return (
+      
       <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        <h2>Favorite Movies</h2>
-		 {profiles.map(profile => {
+        <ul>
+          <h2>Favorite Movies</h2>
+
+         
+          {profiles.map(profile => {
             const userName = users[profile.userID].name;
             const favMovieName = movies[profile.favoriteMovieID].name;
 
             return (
-            <li key={profile.id}>
-                
+             
+              <li key={profile.id}>
+               
                 <p>{`${userName}\'s favorite movie is "${favMovieName}."`}</p>
               </li>
             );
           })}
         </ul>
-      </div>
-            );
-          })}
       </div>
     );
   }
